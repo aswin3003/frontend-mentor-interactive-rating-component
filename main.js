f1bf2e9d6718e84c73168b38511040240a8adb43
A Swinarski <aleksanderswinarski30@gmail.com>
2022-11-01 15:44:14 +0100

    markup revised

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
 console.log("script loaded")
+// const ratingItems=[...(document.querySelectorAll(".rating--item"))]
+// const ratingScore=[...(document.querySelectorAll(".rating--text"))]
+// const scoreMessage=document.querySelector(".rating_confirmation")
+// const submitButton=document.querySelector(".rating--submit")
+
+// console.log(ratingItems)
+// // ratingItems.forEach(e=>e.classList.replace("rating--item","rating--item--selected"))
+
+// for(let count=0;count<ratingItems.length;count++){
+//     ratingItems[count].addEventListener("click",()=>{
+//         ratingItems.forEach(e=>e.classList.replace("rating--item--selected","rating--item"))
+//         ratingItems[count].classList.replace("rating--item","rating--item--selected")
+//         let selectedField=document.querySelector(".rating--item--selected")
+//         return(selectedField.dataset.id)
+
+
+
+        
+//     })
+
+// }
+// // submitButton.addEventListener("click",()=>scoreMessage.innerHTML=`You selected ${score} out of 5`)
+// scoreMessage.innerHTML=`You selected ${selectedField.dataset.id} out of 5`
